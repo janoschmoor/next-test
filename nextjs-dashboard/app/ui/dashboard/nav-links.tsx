ace89211cbe263d20edd8cbbcc0cb752c0a89a5f
Janosch Moor <janosch.moor@gmail.com>
2024-04-04 19:12:15 +0200

    test push to commit to main branch

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -21,6 +21,7 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
+
 export default function NavLinks() {
   const pathname = usePathname();
   return (
