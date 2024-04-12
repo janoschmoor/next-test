a195f5f315a841a20ccd7e8f8fbdb0320aceb4e2
Janosch Moor <janosch.moor@gmail.com>
2024-04-12 13:28:34 +0200

    emoji gone

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -11,7 +11,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard 😉
+        Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
